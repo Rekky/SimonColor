af4e747eabd6c7f8d0a92c801fb32e32ed827ae4
Rekky <ziroustyle@gmail.com>
2014-12-28 04:48:27 +0100

    agregado dialogo temporal de comienzo de partida

--- app/src/main/java/com/ziroudev/simoncolor/simoncolor/Nivel1.java
+++ app/src/main/java/com/ziroudev/simoncolor/simoncolor/Nivel1.java
@@ -1,6 +1,9 @@
 package com.ziroudev.simoncolor.simoncolor;
 
+import android.app.ProgressDialog;
 import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -8,13 +11,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import java.lang.annotation.Target;
+import static android.widget.Toast.LENGTH_SHORT;
 
 
 public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
     //con esta variable sabemos que cololes de botones tenemos
     private int estado;
-    //rango para el random
+
+    //variables de dialogo
+    private ProgressDialog dialog;
+
+    //variables generales
+    private int contador = 3;
     private int max,min,n;
     private Button but1, but2, but3, but4;
 
@@ -22,6 +30,10 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nivel1);
+        //dialogo del contador
+        dialog = new ProgressDialog(this);
+        dialog.setMessage(""+contador);
+        dialog.setCancelable(false);
 
         estado=0;
         but1 = (Button) findViewById(R.id.button1);
@@ -35,7 +47,12 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
         but3.setOnTouchListener(this);
         but4.setOnTouchListener(this);
 
+        //llama al metodo de inciar botones aleatoriamente
         iniciarBotones();
+
+        cuentaAtras();
+
+       // empiezaPartida();
     }
 
     //metodo que retorna sun numero aleatorio entre min y max
@@ -47,10 +64,10 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
     //metodo para iniciar los colores aleatoriamente
     public void iniciarBotones(){
         n = random(0,20);
-        Toast.makeText(this,n+"",Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,n+"", LENGTH_SHORT).show();
         //orden rojo,verde,azul,amarillo
         if(n>=0 && n<5){
-            Toast.makeText(this,"de 0 a 5", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"de 0 a 5", LENGTH_SHORT).show();
             but1.setBackgroundColor(Color.rgb(255, 0, 0));
             but2.setBackgroundColor(Color.rgb(0, 255, 0));
             but3.setBackgroundColor(Color.rgb(0, 0, 255));
@@ -59,7 +76,7 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
         }
         //orden amarillo,azul,verde,rojo
         else if(n>=5 && n<10){
-            Toast.makeText(this,"de 5 a 10", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"de 5 a 10", LENGTH_SHORT).show();
             but1.setBackgroundColor(Color.rgb(255, 255, 0));
             but2.setBackgroundColor(Color.rgb(0, 0, 255));
             but3.setBackgroundColor(Color.rgb(0, 255, 0));
@@ -68,7 +85,7 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
         }
         //orden azul,amarillo,rojo,verde
         else if(n>=10 && n<15){
-            Toast.makeText(this,"de 10 a 15", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"de 10 a 15", LENGTH_SHORT).show();
             but1.setBackgroundColor(Color.rgb(0, 0, 255));
             but2.setBackgroundColor(Color.rgb(255, 255, 0));
             but3.setBackgroundColor(Color.rgb(255, 0, 0));
@@ -77,7 +94,7 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
         }
         //verde,rojo,amarillo,azul
         else if(n>=15 && n<20){
-            Toast.makeText(this,"de 15 a 20", Toast.LENGTH_LONG).show();
+            Toast.makeText(this,"de 15 a 20", LENGTH_SHORT).show();
             but1.setBackgroundColor(Color.rgb(0, 255, 0));
             but2.setBackgroundColor(Color.rgb(255, 0, 0));
             but3.setBackgroundColor(Color.rgb(255, 255, 0));
@@ -88,30 +105,17 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
 
     //metodo que hace una accion al clicar cualquier imageView
     public void onClick(View v){
-
         if(v.getId() == R.id.button1){
-            //but1.setBackgroundColor(Color.rgb(54, 235, 111));
-            //envio el boton1
             onTouch(but1,null);
-
         }
         else if(v.getId() == R.id.button2){
-            //but2.setBackgroundColor(Color.rgb(253,23,23));
-            //envio el boton2
             onTouch(but2,null);
-
         }
         else if(v.getId() == R.id.button3){
-            //but3.setBackgroundColor(Color.rgb(23,253,23));
-            //envio el boton3
             onTouch(but3,null);
-
         }
         else if(v.getId() == R.id.button4){
-            //but4.setBackgroundColor(Color.rgb(245, 3, 4));
-            //envio el boton4
             onTouch(but4,null);
-
         }
     }
 
@@ -217,5 +221,53 @@ public class Nivel1 extends ActionBarActivity implements View.OnTouchListener{
                 }
         }
         return true;
+    }
+
+    //sale el dialogo de la cuenta atras antes de inciar partida
+    public void cuentaAtras(){
+        new CuentaAtras().execute();
+    }
+
+
+    public void empiezaPartida(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                but1.setBackgroundColor(Color.rgb(160, 255, 160));
+            }
+        }, 3000);
+    }
+
+
+    /**
+     * subclase Asyncrona para llevar el dialogo del contador
+     */
+    private class CuentaAtras extends AsyncTask<String, Float, Integer> {
+
+        protected void onPreExecute() {
+            dialog.show(); //Mostramos el diálogo antes de comenzar
+        }
+
+        protected Integer doInBackground(String... urls) {
+            for (int i = 3; i > 0; i--) {
+                //Simulamos cierto retraso
+                try {
+                    Thread.sleep(1000);
+                    contador--;
+                }
+                catch (InterruptedException e){}
+                publishProgress(i/250f);
+
+            }
+            return contador;
+        }
+
+        protected void onProgressUpdate (Float... valores) {
+            dialog.setMessage(""+contador);
+        }
+
+        protected void onPostExecute(Integer bytes) {
+            dialog.dismiss();
+        }
     }
 }
